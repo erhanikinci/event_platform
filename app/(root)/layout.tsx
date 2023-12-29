632b73fbec389fe5d789d57d1cc250fbca8e227b
Erhan İkinci <erhanikinci61@gmail.com>
2023-12-29 15:59:31 +0300

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/shared/Footer"
+import Header from "@/components/shared/Header"
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <div className="flex h-screen flex-col">
+                <Header />
+                <main className="flex-1">{children}</main>
+                <Footer />
+            </div>
+        </html>
+    )
+}
